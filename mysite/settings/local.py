ef9dd09853fad9408d8674b6fa08af80281a4c77
emik_ism <ferguspetr89@gmail.com>
2021-05-18 01:35:15 +0300

    release

--- mysite/settings/local.py
+++
@@ -1,6 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')p!*67p#lu13k!$d-7en5mfqm1-epw+znr9q-7szed^qtp%v1d'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
